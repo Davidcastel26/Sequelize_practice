aaaaf2109b9b8e7c8e467f06c6caea88d2eb9ad4
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-07 16:47:01 -0600

    adding table

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const {Sequelize} = require('sequelize');
+const {Sequelize, DataTypes} = require('sequelize');
+
 
 // connect into a db 
  const sequelize =  new Sequelize('postgres://davidcastellanos:@localhost:5432/seq')
 
  //to confirm that you were able to conect with the local host
  sequelize.authenticate().then(()=>console.log('success')).catch(e=>console.log(e));
+
+//  define(modelName, attributes, options)
+ const Player = sequelize.define('Player',{
+    firstName:{
+        type: DataTypes.STRING
+    },
+    lastName:{
+        type: DataTypes.STRING
+    }
+ },{
+
+ })
+
+ 
