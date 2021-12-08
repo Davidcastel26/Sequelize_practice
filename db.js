562b862027915138c0069193df65950c7831b8d3
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-08 13:45:30 -0600

    adding express

---
+++ db.js
@@ -0,0 +1,47 @@
+const {Sequelize} = require('sequelize');
+const modelPlayer = require('./Models/Player')
+const modelTeam = require('./Models/Team');
+
+// connect into a db 
+const sequelize =  new Sequelize('postgres://davidcastellanos:@localhost:5432/seq'/**we can create an obj here where we can delarte if we would like to get more option */,{
+     //options:
+
+     //default
+     //---logging: console.log,
+
+     //show aditional infromation more than the query SQL
+    //  logging:(...msg) => console.log(msg),
+     
+     //Disable the loggin
+     // logging: false,
+ })
+
+ //to confirm that you were able to conect with the local host
+ sequelize.authenticate().then(()=>console.log('success')).catch(e=>console.log(e));
+
+ //Tables
+ modelPlayer(sequelize);
+ modelTeam(sequelize);
+ //----------------------
+
+//  let p = Player.findByPk(1);
+//  P -> OBJ  => p = {FisrtName, lastNmae, age, .... fullName}
+// p.fullName ---> firstName lastaName
+
+console.log(sequelize.models);
+const {Player, Team} = sequelize.models;
+ //to create all the models in the db
+
+
+//apply all the changes needed to a table to engache with the model
+// Alter = true: UPDATE / false: DO NOT UPDATE
+// Player.sync({alter:true})
+
+//if you would like just create one model follow the sintax below
+// Force = true: CREATE / false: DO NOT CREATE
+// await Model.sync({force:true})
+
+module.exports = {
+    ...sequelize.models,
+    db: sequelize
+}
--- index.js
+++ index.js
@@ -1,42 +1,15 @@
-const {Sequelize} = require('sequelize');
-const modelPlayer = require('./Models/Player')
-const modelTeam = require('./Models/Team');
+const express = require('express');
+const morgan = require('morgan')
+const {db, Player, Team} = require('./db')
 
-// connect into a db 
- const sequelize =  new Sequelize('postgres://davidcastellanos:@localhost:5432/seq'/**we can create an obj here where we can delarte if we would like to get more option */,{
-     //options:
+// db.sync({force:false}); 
 
-     //default
-     //---logging: console.log,
+const server = express();
 
-     //show aditional infromation more than the query SQL
-    //  logging:(...msg) => console.log(msg),
-     
-     //Disable the loggin
-     // logging: false,
- })
+server.use(express.json())
+server.use(morgan('dev'))
 
- //to confirm that you were able to conect with the local host
- sequelize.authenticate().then(()=>console.log('success')).catch(e=>console.log(e));
-
- //Tables
- modelPlayer(sequelize);
- modelTeam(sequelize);
- //----------------------
-
-//  let p = Player.findByPk(1);
-//  P -> OBJ  => p = {FisrtName, lastNmae, age, .... fullName}
-// p.fullName ---> firstName lastaName
-
-console.log(sequelize.models);
-
- //to create all the models in the db
-sequelize.sync({force:true}); 
-
-//apply all the changes needed to a table to engache with the model
-// Alter = true: UPDATE / false: DO NOT UPDATE
-// Player.sync({alter:true})
-
-//if you would like just create one model follow the sintax below
-// Force = true: CREATE / false: DO NOT CREATE
-// await Model.sync({force:true})
+server.listen(3000, () => {
+    console.log('sever listing on port 3000')
+    db.sync({force:true})
+})
