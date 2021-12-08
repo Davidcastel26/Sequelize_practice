b614e84ff8d5fb6da01a699e8b672fd1794ec46c
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-07 19:15:51 -0600

    adding info for the extra colums

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const {Sequelize, DataTypes} = require('sequelize');
 // if the modelName is using upperCase we'll need to use "name"
 //but if the modelName is in lowerCase we do not need to use ""
  const Player = sequelize.define('Player',{
+     //a serail primary key number will be the one who's is automatically created by the id since we did not set as default
     firstName:{
         type: DataTypes.STRING
     },
@@ -18,12 +19,18 @@ const {Sequelize, DataTypes} = require('sequelize');
         type: DataTypes.STRING
     }
  },{
-
+    //  if we do not want to get the colums (CREATE_AT & UPDATE_AT) We could set timestamps as false or true if we would like to get it
+    timestamps: true,//false
+    //if we would like to set our owns names we could do as we are doing right below \\ seting the name with -> ''
+    createAt: 'Created',
+    //and we can chouse if one colum will be at the table or not like we are doing right below
+    updateAt: false
  })
 
  const Team = sequelize.define('Team',{
     code:{
-        type:DataTypes.UUID
+        type:DataTypes.UUID,
+        primaryKey: true
     },
     name:{
         type:DataTypes.STRING
