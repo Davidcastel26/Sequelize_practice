3138572e2f07d3978ac1389a9b21b6ee64942ff0
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-08 11:34:52 -0600

    adding more tools

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const {Sequelize, DataTypes, Model} = require('sequelize');
 
 
 // connect into a db 
- const sequelize =  new Sequelize('postgres://davidcastellanos:@localhost:5432/seq')
+ const sequelize =  new Sequelize('postgres://davidcastellanos:@localhost:5432/seq'/**we can create an obj here where we can delarte if we would like to get more option */,{
+     //options:
+
+     //default
+     //---logging: console.log,
+
+     //show aditional infromation more than the query SQL
+    //  logging:(...msg) => console.log(msg),
+     
+     //Disable the loggin
+     // logging: false,
+ })
 
  //to confirm that you were able to conect with the local host
  sequelize.authenticate().then(()=>console.log('success')).catch(e=>console.log(e));
@@ -17,6 +28,34 @@ const {Sequelize, DataTypes, Model} = require('sequelize');
     },
     lastName:{
         type: DataTypes.STRING
+    },
+    userName:{
+        type: DataTypes.STRING,
+        allowNull: false,//NOT NULL
+        unique: true, //UNIQUE
+
+    },
+    birthday:{
+        type: DataTypes.DATEONLY, //MM-DD-YYYY
+        defaultValue: DataTypes.NOW // data from actual day
+    },
+    age:{
+        type: DataTypes.INTEGER
+    },
+    city:{
+        type: DataTypes.STRING
+    },
+    fullName:{
+        // we won't have the colum fullname
+        //it's a data who is calling by other data
+        type: DataTypes.VIRTUAL,
+        get(){
+            return`${this.firstName} ${this.lastName}`
+        }
+    },
+    country:{
+        //accept only the elemtn into the ENUM
+        type: DataTypes.ENUM('Guatemala','El Salvador','Belice','Nicaragua','Panama','Costa Rica','Honduras')
     }
  },{
     //  if we do not want to get the colums (CREATE_AT & UPDATE_AT) We could set timestamps as false or true if we would like to get it
@@ -42,5 +81,10 @@ const {Sequelize, DataTypes, Model} = require('sequelize');
  //to create all the models in the db
 sequelize.sync({force:true}); 
 
+//apply all the changes needed to a table to engache with the model
+// Alter = true: UPDATE / false: DO NOT UPDATE
+// Player.sync({alter:true})
+
 //if you would like just create one model follow the sintax below
+// Force = true: CREATE / false: DO NOT CREATE
 // await Model.sync({force:true})
