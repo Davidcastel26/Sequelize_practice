35b77d59815c6951b011ed968e2728df7ebd29a5
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-08 16:18:17 -0600

    adding queries

--- index.js
+++ index.js
@@ -22,13 +22,62 @@ server.post('/players', async(req,res)=>{
             season,
             country
         })
+
+        console.log(newPlayer.toJSON());
         res.json(newPlayer)
     }catch(e){
         console.log(e);
     }
 })
 
+server.delete('/player', async(req,res)=>{
+    // const jane = await User.create({name:"Jane"});
+    // await jane.destroy()
+}) 
+
 server.listen(3000, () => {
     console.log('sever listing on port 3000')
     db.sync({force:false})
 })
+
+
+/*
+
+const instancias = await Model.findAll();
+
+//SELECT foo, bar FROM ....
+Model.findAll({
+    attributes:['foo',]
+})
+
+//SELECT foo, bar as baz FROM ....
+Model.findAll({
+    attirbutes:['foo',['bar','baz']]
+})
+
+//Exclude some attribute
+Model.findAll({
+    attributes: {exclude:['baz']}
+})
+---------------------------------------------------------
+
+const instances = Model.findAll({
+    where:{
+        clothe:'orange'
+        status:'good'
+    }
+})
+---------------------------------------------------------
+
+const {Op} = require{"sequelize"};
+Model.findAll({
+    where:{
+        [Op.and]:[
+            {clothe: 'orange'},
+            {status:'good'}
+        ]
+    }
+})
+
+
+*/
